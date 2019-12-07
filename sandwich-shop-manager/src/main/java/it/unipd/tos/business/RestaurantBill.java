311e98cddc93eeed5c12344b607bd0400dfe54b3
Gabriel Ciulei <ciulei.gabriel@gmail.com>
2019-12-07 14:15:36 +0100

    aggiunta logica dello sconto 50

--- sandwich-shop-manager/src/main/java/it/unipd/tos/business/RestaurantBill.java
+++ sandwich-shop-manager/src/main/java/it/unipd/tos/business/RestaurantBill.java
@@ -6,6 +6,7 @@ package it.unipd.tos.business;
 
 import java.util.List;
 import it.unipd.tos.model.MenuItem;
+import it.unipd.tos.model.MenuItem.ItemType;
 import it.unipd.tos.business.TakeAwayBill;
 import it.unipd.tos.business.exception.RestaurantBillException;
 
@@ -17,6 +18,34 @@ public class RestaurantBill implements TakeAwayBill {
         for(MenuItem item : itemsOrdered) {
             total += item.getPrice();
         }
+
+        total -= totalDiscounts(itemsOrdered);
         return total;
     }
+
+    private double totalDiscounts(List<MenuItem> itemsOrdered) {
+        double discounts = 0;
+        discounts += discount50OnType(itemsOrdered, MenuItem.ItemType.PANINO, 5);
+
+        return discounts;
+    }
+
+    private double discount50OnType(List<MenuItem> itemsOrdered, MenuItem.ItemType type, int count) {
+        double cheapest = 0;
+        int typesCount = 0;
+        for(MenuItem item : itemsOrdered) {
+            if (item.getItemType() == type) {
+                typesCount++;
+                if (item.getPrice() < cheapest) {
+                    cheapest = item.getPrice();
+                }
+            } 
+        }
+
+        if (typesCount >= 5) {
+            return cheapest/2;
+        }
+        return 0;
+    }
+
 }
